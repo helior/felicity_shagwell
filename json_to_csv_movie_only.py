e8e87c54ce8163c48cdb0b31bd50e20f86de6f5f
Helior Colorado <me@helior.info>
2023-01-31 21:31:21 -0800

    Remove dependency of writing local files

--- json_to_csv_movie_only.py
+++ json_to_csv_movie_only.py
@@ -1,12 +1,8 @@
-#Set output file and write Movie and UUID
 import boto3
 from botocore.exceptions import NoCredentialsError
 import json
 import csv
-import io
-from pathlib import Path
-
-base_path = Path(__file__).resolve().parent
+from io import StringIO
 
 # Connect to S3
 s3 = boto3.client("s3")
@@ -23,7 +19,7 @@ except json.decoder.JSONDecodeError:
     print("Error: JSON file is not properly formatted")
     exit()
 except NoCredentialsError:
-    with open(base_path / 'celeb_all_metadata_clean2.json') as celebrity_file:
+    with open('celeb_all_metadata_clean2.json') as celebrity_file:
         data = json.load(celebrity_file)
         
 
@@ -31,6 +27,7 @@ except NoCredentialsError:
 output_file = "celeb_movies_brief.csv"
 headers = ['~id', '~from', '~to', '~label']
 
+# Parse the JSON
 rows = []
 for element in data:
     if not element.get('UUID') or not element.get('Movie'):
@@ -55,17 +52,15 @@ for element in data:
             rows.append(celebrity_row)
 
 # Write CSV file
-with open(base_path / output_file, 'w') as csv_file:
-    writer = csv.DictWriter(csv_file, headers, delimiter="\t")
-    writer.writeheader()
-    writer.writerows(rows)
+pseudo_file = StringIO()
+writer = csv.DictWriter(pseudo_file, headers, delimiter="\t")
+writer.writeheader()
+writer.writerows(rows)
+contents = pseudo_file.getvalue()
 
-# Upload the file to S3
-with open(base_path / output_file, 'rb') as csv_file:
-    try:
-        s3.upload_fileobj(csv_file, "so0050-0673f74da1e9-648353486619-us-west-2-proxy", output_file)
-    except NoCredentialsError:
-        print('No credentials to upload :P')
-        exit()
-
-
+# Upload the file contents to S3
+try:
+    s3.upload_fileobj(contents, "so0050-0673f74da1e9-648353486619-us-west-2-proxy", output_file)
+except NoCredentialsError:
+    print('No credentials to upload :P')
+    exit()
